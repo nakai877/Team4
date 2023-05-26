000e84e0f2f7c6d1905d68fa03c4c905362211f4
nakai877 <yusuke.nakai@enisias.com>
2023-05-26 16:05:53 +0900

    firstcommit

---
+++ src/test/java/com/example/demo/Team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
