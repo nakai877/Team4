fb026b706a0701a205d5c746d9a837afac2e0af5
r-hotta <r-hotta@kokusaiexpress.com>
2023-05-29 13:31:28 +0900

    rosterDaoの変更と関連するjavaクラスの作成

---
+++ src/main/java/com/example/demo/entity/EntForm2.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+public class EntForm2 {
+	private int id;
+	private String name;
+	private String comment;
+	private String yakusyoku;
+	private String busyo;
+	private String syumi;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getYakusyoku() {
+		return yakusyoku;
+	}
+	public void setYakusyoku(String yakusyoku) {
+		this.yakusyoku = yakusyoku;
+	}
+	
+	public String getBusyo() {
+		return busyo;
+	}
+	public void setBusyo(String busyo) {
+		this.busyo = busyo;
+	}
+	
+	public String getSyumi() {
+		return syumi;
+	}
+	public void setSyumi(String syumi) {
+		this.syumi = syumi;
+	}
+}
+
+
