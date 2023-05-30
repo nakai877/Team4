3171a43bcd0db2bddecf376b09d563addcbb90c0
r-hotta <r-hotta@kokusaiexpress.com>
2023-05-30 10:11:21 +0900

    タイトル部分の変更

--- src/main/java/com/example/demo/form/ChatController.java
+++ src/main/java/com/example/demo/form/ChatController.java
@@ -17,50 +17,45 @@ import com.example.demo.entity.EntForm2;
 
 @Controller
 public class ChatController {
-	
+
 	@RequestMapping("/form")
 	public String form(Model model) {
-		model.addAttribute("title", "予定確認一覧");
+		model.addAttribute("title", "予定一覧ページ");
 		List<EntForm> list = sampledao.searchDb();
-		model.addAttribute("dbList",list);
+		model.addAttribute("dbList", list);
 		return "form";
-		}
+	}
 
 	//予定入力画面！！
 	@RequestMapping("/add")
 	public String add(Model model, Input form) {
-		model.addAttribute("title", "サンプルフォーム");
+		model.addAttribute("title", "予定入力ページ");
 		return "add";
-		}
-	
+	}
+
 	@RequestMapping("/add2")
 	public String add(Model model, Input2 input2) {
 		return "add2";
 	}
-	
+
 	@RequestMapping("/p")
-	public String form(Model model,Input2 input2) {
-		model.addAttribute("title","プロフィール入力画面");
+	public String form(Model model, Input2 input2) {
+		model.addAttribute("title", "プロフィール入力ページ");
 		return "profile";
 	}
-	
-	
+
 	//名簿一覧画面
 	@RequestMapping("/roster")
 	public String roster(Model model, Input2 input) {
 		//Dao
 		List<EntForm2> list = rosterdao.selectOne(input.getName());
 		List<EntForm> list2 = sampledao.selectOne(input.getName());
-		model.addAttribute("dbList",list);
-		model.addAttribute("dbList2",list2);
-		model.addAttribute("title","名簿詳細画面");
+		model.addAttribute("dbList", list);
+		model.addAttribute("dbList2", list2);
+		model.addAttribute("title", "名簿詳細画面");
 		return "roster";
 	}
-	
-	
-	
-	
-	
+
 	//確認画面
 	@RequestMapping("/confirm")
 	public String confirm(@Validated Input input, BindingResult result, Model model) {
@@ -71,20 +66,21 @@ public class ChatController {
 		}
 
 		model.addAttribute("title", "確認ページ");
-			return "confirm";
-		}
+		return "confirm";
+	}
+
 	@RequestMapping("/confirm2")
 	public String confirm(@Validated Input2 input2, BindingResult result, Model model) {
 
-		if(result.hasErrors()) {
-		model.addAttribute("title","入力ページ");
-		return "profile";
+		if (result.hasErrors()) {
+			model.addAttribute("title", "入力ページ");
+			return "profile";
 		}
 
-		model.addAttribute("title","確認ページ");
+		model.addAttribute("title", "確認ページ");
 		return "confirm2";
 	}
-	
+
 	private SampleDao sampledao = null;
 	private rosterDao rosterdao = null;
 
@@ -93,77 +89,76 @@ public class ChatController {
 		this.sampledao = sampledao;
 		this.rosterdao = rosterdao;
 	}
-	
+
 	//完了の処理
 	@RequestMapping("/complete")
 	public String complete(Model model, Input input) {
-	
+
 		EntForm entform = new EntForm();
-		
+
 		entform.setName(input.getName());
 		entform.setList(input.getList());
-		
-		
+
 		sampledao.insertDb(entform);
-		
-			return "complete";
+
+		return "complete";
 	}
+
 	@RequestMapping("/complete2")
 	public String complete(Model model, Input2 input2) {
-	
+
 		EntForm2 entform = new EntForm2();
-		
+
 		entform.setName(input2.getName());
 		entform.setComment(input2.getComment());
 		entform.setYakusyoku(input2.getYakusyoku());
 		entform.setBusyo(input2.getBusyo());
 		entform.setSyumi(input2.getSyumi());
-		
+
 		rosterdao.insertDb(entform);
-		
-			return "complete2";
+
+		return "complete2";
 	}
-	
-	
+
 	//削除(DELETE)
-		@RequestMapping("/del/{id}")
-		public String destory(@PathVariable Long id) {
-			sampledao.deleteDb(id);
-				return "redirect:/form";
-		}
-		
-		//更新画面の表示(SELECT)
-		@RequestMapping("/edit/{id}")
-		public String editView(@PathVariable Long id, Model model) {
+	@RequestMapping("/del/{id}")
+	public String destory(@PathVariable Long id) {
+		sampledao.deleteDb(id);
+		return "redirect:/form";
+	}
 
-			//DBからデータを1件取ってくる(リストの形)
-			List<EntForm> list = sampledao.selectOne(id);
+	//更新画面の表示(SELECT)
+	@RequestMapping("/edit/{id}")
+	public String editView(@PathVariable Long id, Model model) {
 
-			//リストから、オブジェクトだけをピックアップ
-			EntForm entformdb = list.get(0);
+		//DBからデータを1件取ってくる(リストの形)
+		List<EntForm> list = sampledao.selectOne(id);
 
-			//スタンバイしているViewに向かって、データを投げる
-			model.addAttribute("form", entformdb);
-			model.addAttribute("title", "編集ページ");
-				return "edit";
-		}
-				
-		//更新処理(UPDATE)
-		@RequestMapping("/edit/{id}/exe")
-		public String editExe(@PathVariable Long id, Model model, Input form) {
-					
-			//フォームの値をエンティティに入れ直し
-			EntForm entform = new EntForm();
-			System.out.println(form.getName());//取得できているかの確認
-			System.out.println(form.getList());
-			entform.setName(form.getName());
-			entform.setList(form.getList());
-			
-			//更新の実行
-			sampledao.updateDb(id,entform);
-					
-			//一覧画面へリダイレクト
-			return "redirect:/form";
-		} 
-	
+		//リストから、オブジェクトだけをピックアップ
+		EntForm entformdb = list.get(0);
+
+		//スタンバイしているViewに向かって、データを投げる
+		model.addAttribute("form", entformdb);
+		model.addAttribute("title", "編集ページ");
+		return "edit";
+	}
+
+	//更新処理(UPDATE)
+	@RequestMapping("/edit/{id}/exe")
+	public String editExe(@PathVariable Long id, Model model, Input form) {
+
+		//フォームの値をエンティティに入れ直し
+		EntForm entform = new EntForm();
+		System.out.println(form.getName());//取得できているかの確認
+		System.out.println(form.getList());
+		entform.setName(form.getName());
+		entform.setList(form.getList());
+
+		//更新の実行
+		sampledao.updateDb(id, entform);
+
+		//一覧画面へリダイレクト
+		return "redirect:/form";
+	}
+
 }
